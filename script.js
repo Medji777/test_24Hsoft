066db42e380b70412b92f7b29530f41172068e16
Medji <17dergachev@gmail.com>
2019-10-01 00:48:09 +0300

    Add fix, refactoring and create error handler

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 (()=>{
+    const geoUtil = {
+        midRadius: 6372, // средний радиус Земли
+        RAD: Math.PI / 180, // коэффициент для перевода из градусов широты и долготы в рады.
+        toDistance(x,y,X,Y) {
+           return  this.midRadius * Math.acos(Math.sin(y * this.RAD) * Math.sin(Y * this.RAD) +
+                Math.cos(y * this.RAD) * Math.cos(Y * this.RAD) * Math.cos((X * this.RAD) - (x * this.RAD)))
+        }
+    };
     const api = {
-        getAirplanes(){ // Запрашиваем данные с сервера
-            return fetch('https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=56.84,55.27,33.48,41.48',{
+        async getAirplanes(){ // Запрашиваем данные с сервера
+            let res = await  fetch('https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=56.84,55.27,33.48,41.48',{
                 method: 'GET'
-            })
+            });
+            return res.json();
         }
     };
     const view = {
+        _settings: {resetInterval: 4000},
         render(data){  // Отрисовываем информацию в таблице
             let div = document.querySelector('#root');
             let elHTML = '';
@@ -32,17 +42,46 @@
             });
             elHTML += `</table>`;
             div.innerHTML = elHTML;
+        },
+        showError(err){
+            let div = document.querySelector('#root');
+            let body = div.parentElement;
+            let divError = document.querySelector('.error');
+            if(!divError){
+                this._createError(body,div,err);
+            } else {
+                this._removeErrorElement(divError);
+            }
+        },
+        _createError(body,div,err){
+            let elDiv = document.createElement('div');
+            elDiv.classList.add('error');
+            body.insertBefore(elDiv, div);
+            this._createTextError(err);
+        },
+        _createTextError(err){
+            let divError = document.querySelector('.error');
+            let elDiv = document.createElement('div');
+            elDiv.innerText = err;
+            divError.style.background = '#ff8c8c';
+            divError.appendChild(elDiv);
+            this._resetError(divError);
+        },
+        _removeErrorElement(el){
+            el.remove()
+        },
+        _resetError(el){
+            setTimeout(()=>{this._removeErrorElement(el)},this._settings.resetInterval)
         }
     };
     const app = {
+        _settings: {refreshInterval: 5000},
         _state: {
             initPlanes: null,
             airplanes: [],
             tableHead: ['Номер рейса', 'lat','lng','Скорость, (км/ч)','Курс, (°)','Высота полета, (м)','Вылет','Посадка'],
-            ratioSpeed: 1.875,
-            ratioHeight: 3.281,
-            midRadius: 6372,
-            refreshInterval: 5000,
+            ratioSpeed: 1.875, //  коэффициент перевода из узлов в км.ч
+            ratioHeight: 3.281, // коэффициент перевода из футов в м
             airport: {
                 lat: 55.410307,
                 lng: 37.902451
@@ -50,10 +89,16 @@
         },
         getState(){
             return this._state
-        }, // Для доступа к state из вне и возможной расширяемости
+        },
         async init() {
             await this.run();
-            setInterval(this.run.bind(this), this._state.refreshInterval);
+            this._startInterval();
+        },
+        async _startInterval() {
+            setTimeout(async () => {
+                await this.run();
+                this._startInterval();
+            }, this._settings.refreshInterval);
         },
         async run(){
             await this.setPlanes();
@@ -62,10 +107,9 @@
             view.render(this._state);
         },
         getPlanes(){  // Создаем итоговый объект с выводом информации
-            let {initPlanes,airport,midRadius,ratioSpeed,ratioHeight} = this._state;
+            let {initPlanes,airport,ratioSpeed,ratioHeight} = this._state;
             let Y = airport.lat;
             let X = airport.lng;
-            let RAD = Math.PI / 180; // коэффициент для перевода в рады.
             let airplanes = [];
             for(let i in initPlanes) {
                 if (initPlanes.hasOwnProperty(i)) {
@@ -81,9 +125,7 @@
                         out: airplane[11],
                         in: airplane[12],
                         // distance(ds) - расстояние от самолета до аэропорта
-                        ds: midRadius * Math.acos(Math.sin(airplane[1] * RAD) * Math.sin(Y * RAD) +
-                                Math.cos(airplane[1] * RAD) * Math.cos(Y * RAD) * Math.cos((X * RAD) -
-                                (airplane[2] * RAD)))
+                        ds: geoUtil.toDistance(airplane[2],airplane[1],X,Y)
                     })
                 }
             }
@@ -97,11 +139,11 @@
         async setPlanes(){
             try {
                 let res = await api.getAirplanes();
-                res = await res.json();
                 this._state = {...this._state, initPlanes: {...res}}; // Получаем данные с сервера и записываем в initPlanes
             }
             catch (e) {
-                console.error(e.message)
+                //console.error(e.message);
+                view.showError("some error has occurred");
             }
         }
 
